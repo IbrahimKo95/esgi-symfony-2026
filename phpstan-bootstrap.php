dfa2ddc61cb08d271f3ae617a3d3e7858404b07b
IbrahimKo95 <ibrahkonate95@gmail.com>
2026-07-06 11:18:55 +0200

    fix CI

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once __DIR__.'/vendor/autoload.php';
+
+if (class_exists(Dotenv::class) && file_exists(__DIR__.'/.env')) {
+    (new Dotenv())->bootEnv(__DIR__.'/.env');
+}
 
 $env = $_SERVER['APP_ENV'] ?? 'dev';
 $kernel = new Kernel($env, $env !== 'prod');
